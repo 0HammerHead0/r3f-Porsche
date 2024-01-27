6a1b882cc3db2fec857759d1d914f94c0ad13912
0HammerHead0 <shashwatakahammer@gmail.com>
2024-01-28 01:00:37 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import studio from '@theatre/studio'
+import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
 
 // studio.extend(extension)
 // studio.initialize()
 // Vite
-// if (import.meta.env.DEV) {
-//   studio.initialize()
-//   studio.extend(extension)
-// }
+if (import.meta.env.DEV) {
+  studio.initialize()
+  studio.extend(extension)
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Suspense fallback={null}>
